d08d87e1d0c9aa31d078680c1a5cce9c7ec9f3d3
seulbinim <skfo0827@naver.com>
2024-07-08 01:55:18 +0900

    💄 style
    - html, css 수정
    - scss 적용
    - BEM class 적용
    - image 경로 변경

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,37 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+
   build: {
-    outDir:'docs',
+    outDir: 'docs',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        
+        main: resolve(__dirname, 'src/index.html'),
       },
     },
-    
   },
-})
+
+  server: {
+    port: 3000,
+    open: false,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "@/styles/scss/main";
+      `,
+      },
+      modules: {
+        scopeBehaviour: 'local',
+      },
+    },
+  },
+});
