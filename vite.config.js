a194ffe49b6f7bfde5b4c07d02a22b8758bd3cc6
EraMorgett4 <hsw1568@naver.com>
2024-07-07 16:56:59 +0900

    초기세팅 수정 : 기본 scss 파일들 정리, utilities -> utils 폴더명 변경, vite.config.js alias 추가, package.json scss스크립트 명령어 변경

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'Trinity/src'),
+    },
+  },
+
   build: {
-    outDir:'docs',
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        
       },
     },
-    
   },
-})
+});
