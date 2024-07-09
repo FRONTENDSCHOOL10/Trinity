a5d1e2aa2950067224d1fe168780ffbbd21468de
Lee-Jinyong <yjy1480@gmail.com>
2024-07-09 10:46:59 +0900

    feat: 각 콘텐츠 슬라이드의 스크립트를 하나의 파일에 import

---
+++ src/components/contentSlider/contentSlider.js
@@ -0,0 +1,5 @@
+import '/components/contentSlider/live/live.js';
+import '/components/contentSlider/must/must.js';
+import '/components/contentSlider/only/only.js';
+import '/components/contentSlider/popular/popular.js';
+import '/components/contentSlider/quick/quick.js';
