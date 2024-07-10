76916c65ef90f4d60b02d18029c34ad5657b6eba
Lee-Jinyong <yjy1480@gmail.com>
2024-07-10 20:18:20 +0900

    modi: 콘텐츠 슬라이더 스크립트 파일 수정

--- src/components/contentSlider/contentSlider.js
+++ src/components/contentSlider/contentSlider.js
@@ -1,5 +1,8 @@
-import '/components/contentSlider/live/live.js';
-import '/components/contentSlider/must/must.js';
-import '/components/contentSlider/only/only.js';
-import '/components/contentSlider/popular/popular.js';
-import '/components/contentSlider/quick/quick.js';
+// import '/src/components/contentSlider/live/live';
+// import '/src/components/contentSlider/must/must';
+// import '/src/components/contentSlider/only/only';
+// import '/src/components/contentSlider/popular/popular';
+// import '/src/components/contentSlider/quick/quick';
+import '/src/components/contentSlider/horizontalSlider';
+import '/src/components/contentSlider/verticalSlider';
+import '/src/components/contentSlider/onlySlider';
